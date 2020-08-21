6e428a20c93e98d1e98cf40a4c03d97cdeadbe01
Rob <rbhouck32@gmail.com>
2020-08-21 00:05:36 -0400

    this commit includes some refactoring of code to help implement the client side authentication of the application as well as the ability to sign up a new user.

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <h2>THIS IS THE DASHHBOARD *WINK WINK*</h2>
+    </>
+  );
+};
+
+export default Dashboard;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,162 +1,179 @@
-import React, { useState, useEffect } from 'react';
-import * as yup from 'yup';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import axios from "axios";
 
 const SignUp = () => {
+  const [user, setUser] = useState([]);
 
-    const [user, setUser] = useState([])
+  const [serverError, setServerError] = useState("");
 
-    const [serverError, setServerError] = useState('')
-    
-    const [formState, setFormState] = useState({
+  const [credentials, setCredentials] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    username: "",
+    password: "",
+  });
 
-        firstName: '',
-        lastName: '',
-        email: '',
-        username: '',
-        password: ''
-    })
+  const [errors, setErrors] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    username: "",
+    password: "",
+  });
 
-    const [errors, setErrors] = useState({
+  const validateChange = (e) => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: "",
+        });
+      })
+      .catch((err) => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0],
+        });
+      });
+  };
 
-        firstName: '',
-        lastName: '',
-        email: '',
-        username: '',
-        password: ''
-    })
+  const [buttonDisabled, setButtonDisabled] = useState(true);
 
-    const validateChange = e => {
-        yup
-        .reach(formSchema, e.target.name)
-        .validate(e.target.value)
-        .then(valid => {
-            setErrors({
-                ...errors,
-                [e.target.name]: ''
-            });
-        })
-        .catch(err => {
-            setErrors({
-                ...errors,
-                [e.target.name]: err.errors[0]
-            });
-        })
+  const formSchema = yup.object().shape({
+    fname: yup.string().required("Please enter your first name."),
+    lname: yup.string().required("Please enter your last name."),
+    email: yup.string().required("Please enter a valid email address."),
+    username: yup.string().required("Create a username."),
+    password: yup.string().required("Create a password."),
+  });
+
+  useEffect(() => {
+    formSchema.isValid(credentials).then((valid) => {
+      setButtonDisabled(!valid);
+    });
+  }, [credentials]);
+
+  const inputChange = (e) => {
+    e.persist();
+
+    const newFormData = {
+      ...credentials,
+      [e.target.name]: e.target.value,
     };
 
-    const [buttonDisabled, setButtonDisabled] = useState(true)
+    validateChange(e);
+    setCredentials(newFormData);
+  };
 
-    const formSchema = yup.object().shape({
-        
-        firstName: yup
-        .string()
-        .required('Please enter your first name.'),
-        lastName: yup
-        .string()
-        .required('Please enter your last name.'),
-        email: yup
-        .string()
-        .required('Please enter a valid email address.'),
-        username: yup
-        .string()
-        .required('Create a username.'),
-        password: yup
-        .string()
-        .required('Create a password.')
-    })
+  const userSubmit = (e) => {
+    e.preventDefault();
 
-    useEffect(() => {
-        formSchema.isValid(formState).then(valid => {
-            setButtonDisabled(!valid)
+    axios
+      .post(
+        "https://webpt15-sleep-tracker-api.herokuapp.com/api/auth/register",
+        credentials
+      )
+      .then((res) => {
+        setUser(res.data);
+        console.log("success", user);
+        setCredentials({
+          fname: "",
+          lname: "",
+          email: "",
+          username: "",
+          password: "",
         });
-    }, [formState]);
 
-    const inputChange = e => {
-        e.persist();
-         
-        const newFormData = {
-            ...formState,
-            [e.target.name]: e.target.value
-        };
-        
-        validateChange(e);
-        setFormState(newFormData);
-    };
+        setServerError(null);
+      })
+      .catch((err) => {
+        setServerError("There has been an error");
+      });
+  };
 
-    const userSubmit = e => {
-        
-        e.preventDefault()
-        
-        axios
-        .post('https://webpt15-sleep-tracker-api.herokuapp.com/', formState)
-        .then(res => {
-            setUser(res.data);
-            console.log('success', user);
-            setFormState({
-                firstName: '',
-                lastName: '',
-                email: '',
-                username: '',
-                password: ''
-            })
-
-            setServerError(null);
-        })
-        .catch(err => {
-            setServerError('There has been an error');
-        });
-    }
-
-    return (
-        <form onSubmit={userSubmit}>
-            {serverError ? <p>{serverError}</p> : null }
-            <div>
-                <label htmlFor='firstName'>
-                    <div>First Name</div>
-                    <input id='firstName' name='firstName' type='text' value={formState.firstName} onChange={inputChange}/>
-                    {errors.firstName.length > 0 ? <p>{errors.firstName}</p> : null}
-                </label>
-            </div>
-            <br/>
-            <br/>
-            <div>
-                <label htmlFor='lastName'>
-                    <div>Last Name</div>
-                    <input id='lastName' name='lastName' type='text' value={formState.lastName} onChange={inputChange}/>
-                    {errors.lastName.length > 0 ? <p>{errors.lastName}</p> : null}
-                </label>
-            </div>
-            <br/>
-            <br/>
-            <div>
-                <label htmlFor='email'>
-                    <div>Email</div>
-                    <input id='email' name='email' type='email' value={formState.email} onChange={inputChange}/>
-                    {errors.email.length > 0 ? <p>{errors.email}</p> : null}
-                </label>
-            </div>
-            <br/>
-            <br/>
-            <div>
-                <label htmlFor='username'>
-                    <div>Create a Username</div>
-                    <input id='username' name='username' type='text' value={formState.username} onChange={inputChange}/>
-                    {errors.username.length > 0 ? <p>{errors.username}</p> : null}
-                </label>
-            </div>
-            <br/>
-            <br/>
-            <div>
-                <label htmlFor='password'>
-                    <div>Create a Password</div>
-                    <input id='password' name='password' type='text' value={formState.password} onChange={inputChange}/>
-                    {errors.password.length > 0 ? <p>{errors.password}</p> : null}
-                </label>
-            </div>
-            <button disabled={buttonDisabled}>Sign Up</button>
-        </form>
-    )
-
+  return (
+    <form onSubmit={userSubmit}>
+      {serverError ? <p>{serverError}</p> : null}
+      <div>
+        <label htmlFor="firstName">
+          <div>First Name</div>
+          <input
+            id="firstName"
+            name="fname"
+            type="text"
+            value={credentials.fname}
+            onChange={inputChange}
+          />
+          {errors.fname.length > 0 ? <p>{errors.fname}</p> : null}
+        </label>
+      </div>
+      <br />
+      <br />
+      <div>
+        <label htmlFor="lastName">
+          <div>Last Name</div>
+          <input
+            id="lastName"
+            name="lname"
+            type="text"
+            value={credentials.lname}
+            onChange={inputChange}
+          />
+          {errors.lname.length > 0 ? <p>{errors.lname}</p> : null}
+        </label>
+      </div>
+      <br />
+      <br />
+      <div>
+        <label htmlFor="email">
+          <div>Email</div>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={credentials.email}
+            onChange={inputChange}
+          />
+          {errors.email.length > 0 ? <p>{errors.email}</p> : null}
+        </label>
+      </div>
+      <br />
+      <br />
+      <div>
+        <label htmlFor="username">
+          <div>Create a Username</div>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            value={credentials.username}
+            onChange={inputChange}
+          />
+          {errors.username.length > 0 ? <p>{errors.username}</p> : null}
+        </label>
+      </div>
+      <br />
+      <br />
+      <div>
+        <label htmlFor="password">
+          <div>Create a Password</div>
+          <input
+            id="password"
+            name="password"
+            type="text"
+            value={credentials.password}
+            onChange={inputChange}
+          />
+          {errors.password.length > 0 ? <p>{errors.password}</p> : null}
+        </label>
+      </div>
+      <button disabled={buttonDisabled}>Sign Up</button>
+    </form>
+  );
 };
 
 export default SignUp;
