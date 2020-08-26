d695255948b6fbf115620b6e97fa0255e7446333
Rob <rbhouck32@gmail.com>
2020-08-26 19:31:36 -0400

    This commit includes a heavy refactor of the Journal Entry form. Using Moment.js to gather the unix time stamp. Also getting the emoji rating scale somewhat completed

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,101 +1,182 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { useForm } from "react-hook-form";
-import { date } from "yup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import moment from "moment";
+import {
+  faAngry,
+  faMeh,
+  faSmile,
+  faGrinStars,
+} from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
 
 const EntryForm = () => {
   const [entry, setEntry] = useState([]);
   const [entryData, setEntryData] = useState({
-    date_start: new Date(),
-    date_end: new Date(),
-    sleep_start: "",
-    sleep_end: "",
-    sleep_start_mood: "",
-    sleep_end_mood: "",
+    sleep_start: "null",
+    sleep_end: null,
+    user_id: 1,
+    moods: {
+      before_sleep: "",
+      after_sleep: "",
+      daytime: "",
+    },
   });
-  const { handleSubmit } = useForm();
 
-  const [isLoading, setIsLoading] = useState(false);
-  // const [startTime, setStartTime] = useState(null);
-  // const [endTime, setEndTime] = useState(null);
-  const [overallMood, setOverallMood] = useState(null);
-  // const [chosenEmoji, setChosenEmoji] = useState(null);
+  const { handleSubmit, register } = useForm();
 
-  // const onEmojiClick = (event, emojiOblect) => {
-  //   setChosenEmoji(emojiOblect);
-  // };
   const onSubmit = (data) => {
-    setIsLoading(true);
+    const date = moment(`${data.date_picker} ${data.time}`).format("X");
+    console.log("formatted", date);
 
-    setTimeout(() => {
-      axiosWithAuth()
-        .post("/api/sleep", data)
-        .then((res) => {
-          setIsLoading(false);
-          setEntry(res.data);
-        })
-        .catch((err) => console.error(err));
-    }, 1000);
+    console.log("THIS IS STUFF THAT SUCKS", data);
+
+    axiosWithAuth()
+      .post("/api/sleep", {
+        sleep_start: date,
+        user_id: data.user_id,
+      })
+      .then((res) => {
+        setEntry(res.data);
+      })
+      .catch((err) => console.error(err));
   };
 
-  const handleDateChange = (date) => {
-    setEntryData({
-      date_start: date,
-    });
-  };
   return (
-    <div className="App">
-      <h1> Sleep Tracker</h1>
-      <div className="container1">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="datediv">
-            <h3>Start Date and Time</h3>
-            <DatePicker
-              selected={entryData.date_start}
-              onChange={handleDateChange}
-              dateFormat="MM/dd/yyyy"
-              minDate={new Date()}
-              isClearable
-              showYearDropdown
-              scrollableYearDropdown
+    <CardContainer>
+      <h2>Sleep Journal Entry </h2>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="datediv">
+          <h3>Start Date and Time</h3>
+          <input
+            id="date_picker"
+            ref={register}
+            type="date"
+            name="date_picker"
+            placeholder="HH:MM"
+          />
+
+          <label htmlFor="time">Time: </label>
+          <input
+            name="time"
+            type="time"
+            defaultValue={entryData.sleep_start}
+            ref={register}
+          />
+          <Container className="container">
+            <EmojiWrap className="emoji-wrapper">
+              <FontAwesomeIcon icon={faAngry} />
+              <input type="radio" id="angry" name="angry" value={1} />
+            </EmojiWrap>
+            <EmojiWrap className="emoji-wrapper">
+              <FontAwesomeIcon icon={faMeh} />
+              <input type="radio" id="meh" name="meh" value={2} />
+            </EmojiWrap>
+            <EmojiWrap className="emoji-wrapper">
+              <FontAwesomeIcon icon={faSmile} />
+              <input type="radio" id="happy" name="happy" value={3} />
+            </EmojiWrap>
+            <EmojiWrap className="emoji-wrapper">
+              <FontAwesomeIcon icon={faGrinStars} />
+              <input
+                type="radio"
+                id="really_happy"
+                name="really_happy"
+                value={4}
+              />
+            </EmojiWrap>
+          </Container>
+        </div>
+        <div className="datediv">
+          <h3>End Date and Time</h3>
+          <input
+            id="date_picker"
+            ref={register}
+            type="date"
+            name="date_picker"
+            placeholder="HH:MM"
+          />
+
+          <label htmlFor="time">Time: </label>
+          <input
+            name="time"
+            type="time"
+            defaultValue={entryData.sleep_start}
+            ref={register}
+          />
+          <Container className="container">
+            <EmojiWrap className="emoji-wrapper">
+              <FontAwesomeIcon icon={faAngry} />
+              <input type="radio" id="angry" name="angry" value={1} />
+            </EmojiWrap>
+            <EmojiWrap className="emoji-wrapper">
+              <FontAwesomeIcon icon={faMeh} />
+              <input type="radio" id="meh" name="meh" value={2} />
+            </EmojiWrap>
+            <EmojiWrap className="emoji-wrapper">
+              <FontAwesomeIcon icon={faSmile} />
+              <input type="radio" id="happy" name="happy" value={3} />
+            </EmojiWrap>
+            <EmojiWrap className="emoji-wrapper">
+              <FontAwesomeIcon icon={faGrinStars} />
+              <input
+                type="radio"
+                id="really_happy"
+                name="really_happy"
+                value={4}
+              />
+            </EmojiWrap>
+          </Container>
+        </div>
+        <h3>Overall mood For the Day</h3>
+        <Container className="container">
+          <EmojiWrap className="emoji-wrapper">
+            <FontAwesomeIcon icon={faAngry} />
+            <input type="radio" id="angry" name="angry" value={1} />
+          </EmojiWrap>
+          <EmojiWrap className="emoji-wrapper">
+            <FontAwesomeIcon icon={faMeh} />
+            <input type="radio" id="meh" name="meh" value={2} />
+          </EmojiWrap>
+          <EmojiWrap className="emoji-wrapper">
+            <FontAwesomeIcon icon={faSmile} />
+            <input type="radio" id="happy" name="happy" value={3} />
+          </EmojiWrap>
+          <EmojiWrap className="emoji-wrapper">
+            <FontAwesomeIcon icon={faGrinStars} />
+            <input
+              type="radio"
+              id="really_happy"
+              name="really_happy"
+              value={4}
             />
-            <input type="time" defaultValue={entryData.sleep_start}></input>
-          </div>
-          {/* <div className="datediv">
-            <h3>End Date and Time</h3>
-            <DatePicker
-              selected={entryData.date_end}
-              onChange={(date) => setEntryData(entryData.date_end)}
-              dateFormat="MM/dd/yyyy"
-              minDate={new Date()}
-              isClearable
-              showYearDropdown
-              scrollableYearDropdown
-            />
-            <input type="time" value={entryData.sleep_end}></input>
-          </div>
-          <div className="datediv lastdiv">
-            <h3>Overall Mood Throughout Day</h3>
-            <DatePicker
-              selected={overallMood}
-              onChange={(date) => setOverallMood(date)}
-              dateFormat="MM/dd/yyyy"
-              minDate={new Date()}
-              isClearable
-              showYearDropdown
-              scrollableYearDropdown
-            />
-            <input type="time"></input>
-          </div> */}
-          <button type="submit">Submit Entry</button>
-        </form>
-      </div>
-    </div>
+          </EmojiWrap>
+        </Container>
+        <button type="submit">Submit Entry</button>
+      </form>
+    </CardContainer>
   );
 };
 
 export default EntryForm;
+
+const Container = styled.div`
+  display: flex;
+  margin-top: 25px;
+  justify-content: center;
+`;
+
+const EmojiWrap = styled.div`
+  margin: 0px 10px;
+  margin-bottom: 20px;
+`;
+
+const CardContainer = styled.div`
+  border: 1px solid black;
+  margin: 25px;
+  padding: 10px;
+  text-align: center;
+`;
