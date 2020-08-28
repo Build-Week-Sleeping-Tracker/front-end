f6c9562ada00c9c47f57018fc1ef665baea72387
Panny Lewis <pannylewis@gmail.com>
2020-08-28 16:34:51 -0500

    no changes yet, just added 2 components for possible edit and delete features.

--- src/components/SleepList.js
+++ src/components/SleepList.js
@@ -1,17 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { fetchSleep } from "../actions";
 import SleepCard from "./SleepCard";
+import { useForm, Controller } from "react-hook-form";
 
 const SleepList = (props) => {
   console.log("this is props sleeplist.js", props);
+
   useEffect(() => {
     props.fetchSleep();
   }, []);
 
   return (
     <div>
-      {props.entries.map((entry) => (
+      {props.entries.map((entry, index) => (
         <SleepCard entry={entry} />
       ))}
     </div>
