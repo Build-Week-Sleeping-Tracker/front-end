912b6fe797ee666c3d0dac4c83f08a60074048c1
Rob <rbhouck32@gmail.com>
2020-08-31 08:50:25 -0400

    this commit fixed the issue in which the page would not reload updated posts after a delete of an entry

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import EntryForm from "./EntryForm";
 
@@ -9,21 +9,11 @@ import { fetchSleep } from "../actions";
 
 const Dashboard = () => {
   const { entries, stateError, isFetching } = useSelector((state) => state);
-  const [fetched, setFetched] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const handleFetch = async () => {
-      await dispatch(fetchSleep());
-      setFetched(!fetched);
-    };
-
-    handleFetch();
-
-    return () => handleFetch();
-  }, []);
-
-  useEffect(() => {}, [fetched]);
+    dispatch(fetchSleep());
+  }, [dispatch]);
 
   if (isFetching) return <h2>Loading Entries...</h2>;
   else if (!isFetching && stateError) return <h2>{stateError}</h2>;
